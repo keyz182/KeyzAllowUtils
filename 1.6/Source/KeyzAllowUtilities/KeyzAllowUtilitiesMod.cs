344a4df83e1333d5e2f2daef0c0504bae8829ac0
Kieran David Evans <keyz182@gmail.com>
2025-07-12 19:14:02 +0100

    Cookie cutter initial commit

---
+++ 1.6/Source/KeyzAllowUtilities/KeyzAllowUtilitiesMod.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace KeyzAllowUtilities;
+
+public class KeyzAllowUtilitiesMod : Mod
+{
+    public static Settings settings;
+
+    public KeyzAllowUtilitiesMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.KeyzAllowUtilities.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "KeyzAllowUtilities_SettingsCategory".Translate();
+    }
+}
