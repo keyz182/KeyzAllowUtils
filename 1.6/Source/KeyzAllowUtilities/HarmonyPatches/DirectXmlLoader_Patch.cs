d0d8e67f56faa98cd03bad61e31a05b68cf7205d
Kieran David Evans <keyz182@gmail.com>
2025-07-20 15:32:26 +0100

    feat: add patch for improved def error logging
    - Created `DirectXmlLoader_Patch` class in `KeyzAllowUtilities.HarmonyPatches` namespace to enhance def error logging.
    - Implemented pre and post-fix methods to capture detailed def error sources, including mod name and file path.
    - Integrated lazy-loaded reflection for relevant `Log` properties to enhance performance.
    - Ensured compatibility with existing logging systems by resetting the message count if it exceeds 900.
    - Enhanced log output with detailed information about the def source for improved debugging.

---
+++ 1.6/Source/KeyzAllowUtilities/HarmonyPatches/DirectXmlLoader_Patch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace KeyzAllowUtilities.HarmonyPatches;
+
+[HarmonyPatch(typeof(DirectXmlToObjectNew))]
+public static class DirectXmlLoader_Patch
+{
+    public static Lazy<FieldInfo> MessageQueue = new Lazy<FieldInfo>(()=>AccessTools.Field(typeof(Log), "messageQueue"));
+    public static Lazy<FieldInfo> MessageCount = new Lazy<FieldInfo>(()=>AccessTools.Field(typeof(Log), "messageCount"));
+    public static Lazy<FieldInfo> LastMessage = new Lazy<FieldInfo>(()=>AccessTools.Field(typeof(LogMessageQueue), "lastMessage"));
+
+    [HarmonyPatch(nameof(DirectXmlToObjectNew.DefFromNodeNew))]
+    [HarmonyPrefix]
+    public static void DefFromNodeNew_Prefix(ref LogMessage __state)
+    {
+        LogMessage message = LastLogMessage();
+        if(message == null) return;
+        __state = message;
+    }
+
+    public static LogMessage LastLogMessage()
+    {
+        LogMessageQueue queue = (LogMessageQueue) MessageQueue.Value.GetValue(typeof(Log));
+        if(queue == null) return null;
+        LogMessage message = (LogMessage) LastMessage.Value.GetValue(queue);
+        return message;
+    }
+
+    [HarmonyPatch(nameof(DirectXmlToObjectNew.DefFromNodeNew))]
+    [HarmonyPostfix]
+    public static void DefFromNodeNew_Postfix(Def __result, LoadableXmlAsset loadingAsset, LogMessage __state)
+    {
+        if (!KeyzAllowUtilitiesMod.settings.DefErrorLog || __result == null  || LastLogMessage() == __state)
+        {
+            return;
+        }
+
+        ModContentPack mod = loadingAsset?.mod;
+        if ((int)MessageCount.Value.GetValue(typeof(Log)) > 900)
+        {
+            Log.ResetMessageCount();
+        }
+
+
+        string text = null;
+        if (loadingAsset != null)
+        {
+            string fullFilePath = loadingAsset.FullFilePath;
+            text = fullFilePath?.Replace(mod?.RootDir ?? string.Empty, "");
+        }
+
+        ModLog.DefError($"From Def Named: {__result.defName} | In mod named: {mod?.Name} | At path: {text}");
+    }
+}
