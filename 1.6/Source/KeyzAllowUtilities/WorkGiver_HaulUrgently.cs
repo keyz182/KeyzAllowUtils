42f870760073c817012c8d00a7df42c78777dd5f
Kieran David Evans <keyz182@gmail.com>
2025-07-20 11:12:58 +0100

    feat: optimize urgent hauling selection logic
    - Introduced a buffered search pool for "Haul Urgently" jobs to improve performance by limiting search iterations and randomizing selection.
    - Adjusted `JobOnThingDelegate` initialization for streamlined function handling.

--- 1.6/Source/KeyzAllowUtilities/WorkGiver_HaulUrgently.cs
+++ 1.6/Source/KeyzAllowUtilities/WorkGiver_HaulUrgently.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using RimWorld;
 using Verse;
@@ -9,12 +10,27 @@ namespace KeyzAllowUtilities;
 public class WorkGiver_HaulUrgently: WorkGiver_Scanner
 {
     public delegate Job TryGetJobOnThing(Pawn pawn, Thing t, bool forced);
-    public static TryGetJobOnThing JobOnThingDelegate = (pawn, t, forced) => HaulAIUtility.HaulToStorageJob(pawn, t, forced);
+    public static TryGetJobOnThing JobOnThingDelegate = HaulAIUtility.HaulToStorageJob;
+
 
     public override Danger MaxPathDanger(Pawn pawn) => Danger.Deadly;
 
+    public List<Thing> searchPool = [];
+
+
     public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
     {
+        if (searchPool.NullOrEmpty())
+        {
+            searchPool = pawn.Map.designationManager.SpawnedDesignationsOfDef(KeyzAllowUtilitesDefOf.KAU_HaulUrgentlyDesignation).Where(d => d.target.Thing != null)
+                .Select(d => d.target.Thing).ToList();
+        }
+
+        int toTake = Math.Min(Math.Max(10, searchPool.Count / 10), searchPool.Count);
+
+        IEnumerable<Thing> thingsOut = searchPool.TakeRandom(toTake);
+        searchPool.RemoveAll(t => thingsOut.Contains(t));
+
         return pawn.Map.designationManager.SpawnedDesignationsOfDef(KeyzAllowUtilitesDefOf.KAU_HaulUrgentlyDesignation).Where(d => d.target.Thing != null).Select(d => d.target.Thing);
     }
 
