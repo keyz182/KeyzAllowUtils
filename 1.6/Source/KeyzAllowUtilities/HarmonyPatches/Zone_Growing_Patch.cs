5eaaa22aa9d8662d5b344758dd3b36d4592a9b2b
Kieran David Evans <keyz182@gmail.com>
2026-04-10 10:53:32 +0100

    Performance fix for grow zone designator

--- 1.6/Source/KeyzAllowUtilities/HarmonyPatches/Zone_Growing_Patch.cs
+++ 1.6/Source/KeyzAllowUtilities/HarmonyPatches/Zone_Growing_Patch.cs
@@ -9,21 +9,29 @@ namespace KeyzAllowUtilities.HarmonyPatches;
 [HarmonyPatch(typeof(Zone_Growing))]
 public static class Zone_Growing_Patch
 {
+    private static Designator_ZoneAdd_GrowingFertile_Expand _fertileExpand;
+    private static Game _cachedForGame;
+
     [HarmonyPatch(nameof(Zone_Growing.GetGizmos))]
     [HarmonyPostfix]
     public static void GetGizmos_Patch(ref IEnumerable<Gizmo> __result)
     {
+        if (_cachedForGame != Current.Game)
+        {
+            _fertileExpand = DesignatorUtility.FindAllowedDesignator<Designator_ZoneAdd_GrowingFertile_Expand>();
+            _cachedForGame = Current.Game;
+        }
+        if (_fertileExpand == null) return;
+
         List<Gizmo> gizmos = __result.ToList();
         Gizmo othergizmo = gizmos.FirstOrDefault(g => g is Designator_ZoneAdd_Growing_Expand);
-        Gizmo fertileExpand = DesignatorUtility.FindAllowedDesignator<Designator_ZoneAdd_GrowingFertile_Expand>();
-        if(fertileExpand == null) return;
         if (othergizmo == null)
         {
-            gizmos.Add(fertileExpand);
+            gizmos.Add(_fertileExpand);
         }
         else
         {
-            gizmos.Insert(gizmos.IndexOf(othergizmo)+1, fertileExpand);
+            gizmos.Insert(gizmos.IndexOf(othergizmo) + 1, _fertileExpand);
         }
 
         __result = gizmos;
